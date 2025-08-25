63f66c0a3f5128440a0fde72acfc2f13e5f89803
joaodepaulojp <joaodepaulocardoso@edu.unifor.br>
2025-08-25 20:38:24 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 const Home = () => {
   return (
@@ -27,6 +30,28 @@ const Home = () => {
             className="rounded-xl object-cover"
           />
         </div>
+
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-bold">Corte de Cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/988646ea-dcb6-4f47-8a03-8d4586b7bc21-16v.png" />
+                </Avatar>
+                <p className="text-sm">Instituto de Beleza Joana de Paulo</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">25</p>
+              <p className="text-sm">18:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
