e4ef947b448e6f461690a368c17ade889629ed78
joaodepaulojp <joaodepaulocardoso@edu.unifor.br>
2025-08-27 14:22:01 -0300

    feat: add elements tittle address avaliation and description

--- app/_components/barbershop-item.tsx
+++ app/_components/barbershop-item.tsx
@@ -34,7 +34,7 @@ const BarberShopItem = ({ barberShop }: BarberShopItemProps) => {
         <div className="px-1 py-3">
           <h3 className="truncate font-semibold">{barberShop.name}</h3>
           <p className="truncate text-sm text-gray-400">{barberShop.address}</p>
-          <Button variant="secondary" className="mt-3 w-full">
+          <Button variant="secondary" className="mt-3 w-full" asChild>
             <Link href={`/barbershops/${barberShop.id}`}>Reservar</Link>
           </Button>
         </div>
--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,8 +1,15 @@
 import { db } from "@/app/_lib/prisma"
-import { ChevronLeftIcon, MenuIcon } from "lucide-react"
+import {
+  ChevronLeftIcon,
+  MapIcon,
+  MapPinIcon,
+  MenuIcon,
+  StarIcon,
+} from "lucide-react"
 import Image from "next/image"
 import { Button } from "@/app/_components/ui/button"
 import Link from "next/link"
+import { notFound } from "next/navigation"
 
 interface BarbershopPageProps {
   params: Promise<{ id: string }>
@@ -16,31 +23,58 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
   })
 
   if (!barbershop) {
-    return <p>Ops! Não encontrei nenhuma barbearia...</p>
+    return notFound()
   }
 
   return (
-    <div className="relative h-[250px] w-full">
-      <Image
-        alt={barbershop?.name}
-        src={barbershop?.imageUrl}
-        fill
-        className="object-cover"
-      />
+    <div>
+      <div className="relative h-[250px] w-full">
+        <Image
+          alt={barbershop?.name}
+          src={barbershop?.imageUrl}
+          fill
+          className="object-cover"
+        />
 
-      <Button size="icon" variant="secondary" className="absolute top-4 left-4">
-        <Link href="/">
-          <ChevronLeftIcon />
-        </Link>
-      </Button>
+        <Button
+          size="icon"
+          variant="secondary"
+          className="absolute top-4 left-4"
+          asChild
+        >
+          <Link href="/">
+            <ChevronLeftIcon />
+          </Link>
+        </Button>
 
-      <Button
-        size="icon"
-        variant="secondary"
-        className="absolute top-4 right-4"
-      >
-        <MenuIcon />
-      </Button>
+        <Button
+          size="icon"
+          variant="secondary"
+          className="absolute top-4 right-4"
+        >
+          <MenuIcon />
+        </Button>
+      </div>
+
+      <div className="border-b border-solid p-5">
+        <h1 className="mb-3 text-xl font-bold">{barbershop?.name}</h1>
+        <div className="mb-2 flex items-center gap-2">
+          <MapPinIcon className="fill-primary text-primary" size={18} />
+          <p className="text-sm">{barbershop?.address}</p>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <StarIcon className="fill-primary text-primary" size={18} />
+          <p className="text-sm">4,9 (894 avaliações)</p>
+        </div>
+      </div>
+
+      <div className="space-y-2 border-b border-solid p-5">
+        <h2 className="fonte-bold text-xs text-gray-400 uppercase">
+          Sobre nós
+        </h2>
+        <p className="text-justify text-sm">{barbershop?.description}</p>
+      </div>
     </div>
   )
 }
