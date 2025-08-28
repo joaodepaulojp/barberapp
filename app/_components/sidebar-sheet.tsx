9d1cca86a3d2e73ba55b6574ec458538363eac3f
joaodepaulojp <joaodepaulocardoso@edu.unifor.br>
2025-08-28 12:03:18 -0300

    feat: add sign in with google

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react"
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
 import { QuickSearchOptions } from "../_constants/quickSearch"
@@ -5,93 +7,91 @@ import { Avatar, AvatarImage } from "./ui/avatar"
 import Link from "next/link"
 import { Button } from "./ui/button"
 import Image from "next/image"
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "./ui/dialog"
+import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog"
+import { signIn, signOut, useSession } from "next-auth/react"
+import SignInDialog from "./sign-in-dialog"
 
 const SidebarSheet = () => {
+  const { data } = useSession()
+  const handleLogOutClick = () => signOut()
+
   return (
     <SheetContent>
       <SheetHeader>
         <SheetTitle className="text-left">Menu</SheetTitle>
-
-        <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
-          <h2 className="font-bold">Olá, faça seu login!</h2>
-          <Dialog>
-            <DialogTrigger asChild>
-              <Button size="icon">
-                <LogInIcon />
-              </Button>
-            </DialogTrigger>
-            <DialogContent className="w-[90%]">
-              <DialogHeader>
-                <DialogTitle>Faça login na plataforma!</DialogTitle>
-                <DialogDescription>
-                  Conecte-se com sua conta do Google.
-                </DialogDescription>
-              </DialogHeader>
-
-              <Button variant="outline" className="gap-1 font-bold">
-                <Image src="/google.svg" alt="google" width={18} height={18} />
-                Google
-              </Button>
-            </DialogContent>
-          </Dialog>
-          {/* <Avatar>
-            <AvatarImage src="https://images.unsplash.com/photo-1728577740843-5f29c7586afe?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-          </Avatar>
-
-          <div>
-            <p className="font-bold">João de Paulo</p>
-            <p className="text-xs">joaodepaulo@joaodepaulo.site</p>
-          </div> */}
-        </div>
-
-        <div className="flex flex-col gap-2 border-b border-solid py-4">
-          <SheetClose asChild>
-            <Button className="justify-start gap-2" variant="ghost" asChild>
-              <Link href="/">
-                <HomeIcon size={18} />
-                Inicio
-              </Link>
-            </Button>
-          </SheetClose>
-          <Button className="justify-start gap-2" variant="ghost">
-            <CalendarIcon size={18} />
-            Agendamentos
-          </Button>
-        </div>
-
-        <div className="flex flex-col gap-2 border-b border-solid py-4">
-          {QuickSearchOptions.map((option) => (
-            <Button
-              key={option.title}
-              className="justify-start gap-2"
-              variant="ghost"
-            >
-              <Image
-                src={option.imageUrl}
-                alt={option.title}
-                width={18}
-                height={18}
-              />
-              {option.title}
-            </Button>
-          ))}
-        </div>
-
-        <div className="flex flex-col gap-2 py-4">
-          <Button className="justify-start gap-2" variant="ghost">
-            <LogOutIcon size={18} />
-            Sair da Conta
-          </Button>
-        </div>
       </SheetHeader>
+
+      <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+        {data?.user ? (
+          <div className="ml-3 flex items-center gap-2">
+            <Avatar>
+              <AvatarImage src={data?.user?.image ?? ""} />
+            </Avatar>
+            <div>
+              <p className="font-bold">{data.user.name}</p>
+              <p className="text-xs">{data.user.email}</p>
+            </div>
+          </div>
+        ) : (
+          <>
+            <h2 className="ml-3 font-bold">Olá, faça seu login!</h2>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button size="icon" className="mr-3">
+                  <LogInIcon />
+                </Button>
+              </DialogTrigger>
+              <DialogContent className="w-[90%]">
+                <SignInDialog />
+              </DialogContent>
+            </Dialog>
+          </>
+        )}
+      </div>
+
+      <div className="flex flex-col gap-2 border-b border-solid py-4">
+        <SheetClose asChild>
+          <Button className="justify-start gap-2" variant="ghost" asChild>
+            <Link href="/">
+              <HomeIcon size={18} />
+              Inicio
+            </Link>
+          </Button>
+        </SheetClose>
+        <Button className="justify-start gap-2" variant="ghost">
+          <CalendarIcon size={18} />
+          Agendamentos
+        </Button>
+      </div>
+
+      <div className="flex flex-col gap-2 border-b border-solid py-4">
+        {QuickSearchOptions.map((option) => (
+          <Button
+            key={option.title}
+            className="justify-start gap-2"
+            variant="ghost"
+          >
+            <Image
+              src={option.imageUrl}
+              alt={option.title}
+              width={18}
+              height={18}
+            />
+            {option.title}
+          </Button>
+        ))}
+      </div>
+
+      <div className="flex flex-col gap-2 py-4">
+        <Button
+          className="justify-start gap-2"
+          variant="ghost"
+          onClick={handleLogOutClick}
+        >
+          <LogOutIcon size={18} />
+          Sair da Conta
+        </Button>
+      </div>
     </SheetContent>
   )
 }
---
+++ app/_components/sign-in-dialog.tsx
@@ -0,0 +1,29 @@
+import { signIn } from "next-auth/react"
+import { DialogDescription, DialogHeader, DialogTitle } from "./ui/dialog"
+import { Button } from "./ui/button"
+import Image from "next/image"
+
+const SignInDialog = () => {
+  const handleLoginWithGoogleClick = () => signIn("google")
+  return (
+    <>
+      <DialogHeader>
+        <DialogTitle>Faça login na plataforma!</DialogTitle>
+        <DialogDescription>
+          Conecte-se com sua conta do Google.
+        </DialogDescription>
+      </DialogHeader>
+
+      <Button
+        variant="outline"
+        className="gap-1 font-bold"
+        onClick={handleLoginWithGoogleClick}
+      >
+        <Image src="/google.svg" alt="google" width={18} height={18} />
+        Google
+      </Button>
+    </>
+  )
+}
+
+export default SignInDialog
